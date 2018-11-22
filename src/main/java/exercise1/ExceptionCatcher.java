432074a467605afe9f84031eee4bae0ca77bbbbb
DiegoADAMOLI <diego.adamolicolina@student.umons.ac.be>
2018-11-22 21:13:33 +0100

    ex1 commenté
    Signed-off-by: DiegoADAMOLI <diego.adamolicolina@student.umons.ac.be>

--- src/main/java/exercise1/ExceptionCatcher.java
+++ src/main/java/exercise1/ExceptionCatcher.java
@@ -6,13 +6,18 @@ public class ExceptionCatcher {
 	public ExceptionCatcher(ExceptionThrower exceptionThrower) {
 		this.exceptionThrower = exceptionThrower;
 	}
+	/*méthide déjà fournie telle quel, la variable va être initialisé à la
+	"valeur de l'exception", çà va servir pour le test unitaire*/
 
 	void executeExceptionThrowerSafely(int value) {
 		try {
 			exceptionThrower.throwACustomExceptionWhenValueIs42(value);	//statements that may cause an exception
 		} catch(RuntimeException e) {
-
+			System.out.println("exception attrapée");//on pourrait aussi ne rien faire
 		}
-
 	}
+	/*je try le code qui pourrait poser pb, puis je catch l'excpetion runtime si y'a un
+	pb (ici va y'avoir un pb car la fonction rend une exception quand la value vaut 42),
+	et je fais alors l'action dans le bloc catch (try implique d'office un catch ou un
+	finally, potentiellement les 2*/
 }
--- src/main/java/exercise1/ExceptionThrower.java
+++ src/main/java/exercise1/ExceptionThrower.java
@@ -4,4 +4,6 @@ public class ExceptionThrower {
 	void throwACustomExceptionWhenValueIs42(int value) {
 		if(value==42) throw new RuntimeException();
 	}
+	/*idée : je fais passer un paramètre à cette fct,
+	si ce param vaut 42, alors la fct renvoie une runtime exception à l'appelant*/
 }
